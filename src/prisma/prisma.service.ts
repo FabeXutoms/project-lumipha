23a759f20e3181dbace8c88e1000a17efa40c81a
FabeXutoms <eintepe1496@gmail.com>
2025-12-22 23:12:12 +0300

    12231

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,8 +10,24 @@ import { PrismaClient } from '@prisma/client';
 
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          // BURAYA .env İÇİNDEKİ DATABASE_URL'İ DİREKT YAZIYORUZ
+          url: "mysql://lumipha:Lumipha2025@127.0.0.1:3306/lumipha_db"
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      console.log("✅ Veritabanı bağlantısı başarılı! (3306)");
+    } catch (error) {
+      console.error("❌ Veritabanı bağlantı hatası:", error);
+    }
   }
 
   async onModuleDestroy() {
